9c954903b4e4624c63cb723a6f58749aad519aa9
cocoputiz-sudo <cocoputiz@gmail.com>
2026-09-22 02:12:07 -0300

    release: prepare IMORTAIS Combat Client v0.4.4

--- upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
+++ upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
@@ -4,22 +4,22 @@ using System.Runtime.Versioning;
 using System.Windows;
 using System.Runtime.CompilerServices;
 
-// Allgemeine Informationen Ã¼ber eine Assembly werden Ã¼ber die folgenden
-// Attribute gesteuert. Ã„ndern Sie diese Attributwerte, um die Informationen zu Ã¤ndern,
+// Allgemeine Informationen ÃƒÂ¼ber eine Assembly werden ÃƒÂ¼ber die folgenden
+// Attribute gesteuert. Ãƒâ€žndern Sie diese Attributwerte, um die Informationen zu ÃƒÂ¤ndern,
 // die einer Assembly zugeordnet sind.
 [assembly: AssemblyTitle("IMORTAIS Combat Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IMORTAIS")]
 [assembly: AssemblyProduct("IMORTAIS Combat Client")]
-[assembly: AssemblyCopyright("Copyright Â©  2026")]
+[assembly: AssemblyCopyright("Copyright Ã‚Â©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: SupportedOSPlatform("windows")]
 
 // Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
-// fÃ¼r COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
-// COM aus zugreifen mÃ¼ssen, sollten Sie das ComVisible-Attribut fÃ¼r diesen Typ auf "True" festlegen.
+// fÃƒÂ¼r COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen mÃƒÂ¼ssen, sollten Sie das ComVisible-Attribut fÃƒÂ¼r diesen Typ auf "True" festlegen.
 [assembly: ComVisible(false)]
 
 //Um mit dem Erstellen lokalisierbarer Anwendungen zu beginnen, legen Sie
@@ -27,30 +27,30 @@ using System.Runtime.CompilerServices;
 //in einer <PropertyGroup> fest.  Wenn Sie in den Quelldateien beispielsweise Deutsch
 //(Deutschland) verwenden, legen Sie <UICulture> auf \"de-DE\" fest.  Heben Sie dann die Auskommentierung
 //des nachstehenden NeutralResourceLanguage-Attributs auf.  Aktualisieren Sie "en-US" in der nachstehenden Zeile,
-//sodass es mit der UICulture-Einstellung in der Projektdatei Ã¼bereinstimmt.
+//sodass es mit der UICulture-Einstellung in der Projektdatei ÃƒÂ¼bereinstimmt.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 [assembly: ThemeInfo(
-    ResourceDictionaryLocation.None, //Speicherort der designspezifischen RessourcenwÃ¶rterbÃ¼cher
+    ResourceDictionaryLocation.None, //Speicherort der designspezifischen RessourcenwÃƒÂ¶rterbÃƒÂ¼cher
                                      //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-                                     // oder in den Anwendungsressourcen-WÃ¶rterbÃ¼chern nicht gefunden werden kann.)
-    ResourceDictionaryLocation.SourceAssembly //Speicherort des generischen RessourcenwÃ¶rterbuchs
+                                     // oder in den Anwendungsressourcen-WÃƒÂ¶rterbÃƒÂ¼chern nicht gefunden werden kann.)
+    ResourceDictionaryLocation.SourceAssembly //Speicherort des generischen RessourcenwÃƒÂ¶rterbuchs
                                               //(wird verwendet, wenn eine Ressource auf der Seite nicht gefunden wird,
-                                              // designspezifischen RessourcenwÃ¶rterbuch nicht gefunden werden kann.)
+                                              // designspezifischen RessourcenwÃƒÂ¶rterbuch nicht gefunden werden kann.)
 )]
 
 // Tests
 [assembly: InternalsVisibleTo("StatisticsAnalysisTool.Tests")]
 
-// Versionsinformationen fÃ¼r eine Assembly bestehen aus den folgenden vier Werten:
+// Versionsinformationen fÃƒÂ¼r eine Assembly bestehen aus den folgenden vier Werten:
 //
 //      Hauptversion
 //      Nebenversion
 //      Buildnummer
 //      Revision
 //
-// Sie kÃ¶nnen alle Werte angeben oder Standardwerte fÃ¼r die Build- und Revisionsnummern verwenden,
+// Sie kÃƒÂ¶nnen alle Werte angeben oder Standardwerte fÃƒÂ¼r die Build- und Revisionsnummern verwenden,
 // indem Sie "*" wie unten gezeigt eingeben:
 // [assembly: AssemblyVersion("0.4.4.0")]
 [assembly: AssemblyVersion("0.4.4.0")]
