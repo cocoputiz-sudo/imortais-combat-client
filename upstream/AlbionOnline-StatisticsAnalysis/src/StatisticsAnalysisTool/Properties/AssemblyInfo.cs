798d0b465d49d110bfe1611642a2fce950d0768d
cocoputiz-sudo <cocoputiz@gmail.com>
2026-09-22 08:46:39 -0300

    release: set assembly version 0.4.9

--- upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
+++ upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Properties/AssemblyInfo.cs
@@ -54,5 +54,5 @@ using System.Runtime.CompilerServices;
 // indem Sie "*" wie unten gezeigt eingeben:
 // AssemblyVersion is intentionally stable across 0.4.x releases.
 [assembly: AssemblyVersion("0.4.4.0")]
-[assembly: AssemblyFileVersion("0.4.8.0")]
-[assembly: AssemblyInformationalVersion("0.4.8")]
+[assembly: AssemblyFileVersion("0.4.9.0")]
+[assembly: AssemblyInformationalVersion("0.4.9")]
