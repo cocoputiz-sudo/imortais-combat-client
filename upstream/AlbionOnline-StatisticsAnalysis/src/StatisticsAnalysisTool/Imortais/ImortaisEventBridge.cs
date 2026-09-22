a36696c7adcd8d738b516fa0165aeba895d9f542
cocoputiz-sudo <cocoputiz@gmail.com>
2026-09-22 09:02:46 -0300

    release: mark guild presence probe as v0.5.0

--- upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Imortais/ImortaisEventBridge.cs
+++ upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Imortais/ImortaisEventBridge.cs
@@ -464,7 +464,7 @@ public static class ImortaisEventBridge
                     {
                         deviceId = _config.DeviceId,
                         playerName = _config.PlayerName,
-                        version = "0.4.9"
+                        version = "0.5.0"
                     },
                     ctaEventId = _config.CtaEventId,
                     events = batch
