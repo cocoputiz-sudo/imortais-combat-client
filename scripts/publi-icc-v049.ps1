5029d5d37828008db4420489935da6f86f1b207b
cocoputiz-sudo <cocoputiz@gmail.com>
2026-09-22 09:17:05 -0300

    fix: validate v0.4.9 versions in release script

--- scripts/publi-icc-v049.ps1
+++ scripts/publi-icc-v049.ps1
@@ -161,13 +161,13 @@ $bridgeText = [System.IO.File]::ReadAllText($BridgeFile)
 $configText = [System.IO.File]::ReadAllText($ConfigFile)
 $lootControllerText = [System.IO.File]::ReadAllText($LootControllerFile)
 
-if ($projectText -notmatch '<ApplicationVersion>0\.4\.8\.0</ApplicationVersion>') {
+if ($projectText -notmatch '<ApplicationVersion>0\.4\.9\.0</ApplicationVersion>') {
     throw "csproj não está em v0.4.9."
 }
-if ($assemblyText -notmatch 'AssemblyFileVersion\("0\.4\.8\.0"\)') {
+if ($assemblyText -notmatch 'AssemblyFileVersion\("0\.4\.9\.0"\)') {
     throw "AssemblyFileVersion não está em v0.4.9."
 }
-if ($assemblyText -notmatch 'AssemblyInformationalVersion\("0\.4\.8"\)') {
+if ($assemblyText -notmatch 'AssemblyInformationalVersion\("0\.4\.9"\)') {
     throw "AssemblyInformationalVersion não está em v0.4.9."
 }
 if ($updaterText -match 'if \(!await IsAppCastSignatureTrustedAsync') {
