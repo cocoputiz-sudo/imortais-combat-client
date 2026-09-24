931257b988a2fcd4bad056012a1e550a116de867
cocoputiz-sudo <cocoputiz@gmail.com>
2026-09-24 01:00:24 -0300

    fix: include LINQ for diagnostics activity ordering

--- upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Views/MainWindow.xaml.cs
+++ upstream/AlbionOnline-StatisticsAnalysis/src/StatisticsAnalysisTool/Views/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using StatisticsAnalysisTool.ViewModels;
 using StatisticsAnalysisTool.Imortais;
 using StatisticsAnalysisTool.Updater;
 using System;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Windows;
